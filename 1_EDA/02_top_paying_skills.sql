e62c5e81e16c53e5df28996d5df0ae7dde9f449a
Kathoni <thrusherkeys@gmail.com>
2026-08-17 19:18:40 +0300

    query on query 2 done

--- 1_EDA/02_top_paying_skills.sql
+++ 1_EDA/02_top_paying_skills.sql
@@ -1,12 +1,26 @@
 /*
- Question: What are the highest-paying skills for data engineers?
-  Calculate the median salary for each skill required in data engineer positions
-  Focus on remote positions with specified salaries
-  Include skill frequency to identify both salary and demand
-  Why? 
-  Helps identify which skills command the highest compensation while also showing 
+Question: What are the highest-paying skills for data engineers?
+- Calculate the median salary for each skill required in data engineer positions
+- Focus on remote positions with specified salaries
+- Include skill frequency to identify both salary and demand
+- Why? Helps identify which skills command the highest compensation while also showing 
     how common those skills are, providing a more complete picture for skill development priorities
 */
 
-SELECT *
-FROM job_postings_fact AS jpf;
+SELECT 
+    sd.skills,
+    ROUND(MEDIAN(jpf.salary_year_avg), 0) AS median_salary,
+    COUNT(jpf.*) AS skill_count
+FROM job_postings_fact jpf
+INNER JOIN skills_job_dim sjd ON jpf.job_id = sjd.job_id
+INNER JOIN skills_dim sd ON sjd.skill_id = sd.skill_id
+WHERE
+    jpf.job_title_short = 'Data Engineer'
+    AND jpf.job_work_from_home = True 
+GROUP BY
+    sd.skills
+HAVING
+    COUNT(sd.skills) >= 100
+ORDER BY
+    median_salary DESC
+LIMIT 25;
